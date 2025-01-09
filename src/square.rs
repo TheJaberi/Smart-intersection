270577ec960a02fc6af89dea203bde809018c06a
ExTBH <natheer@extbh.dev>
2025-01-09 20:08:39 +0300

    Follow Valid Routes only

--- src/square.rs
+++ src/square.rs
@@ -15,13 +15,23 @@ pub struct Square {
     turn_y: Option<i32>,
 }
 
+#[derive(Debug)]
+struct CalculatedCoordinates {
+    starting_x: i32,
+    starting_y: i32,
+    turn_x: Option<i32>,
+    turn_y: Option<i32>,
+}
+
 impl Square {
     pub fn new(
         x: i32,
         y: i32,
-        initial_position: Direction,
+        initial_direction: Direction,
         target_direction: Direction,
         size: u32,
+        turn_x: Option<i32>,
+        turn_y: Option<i32>,
     ) -> Self {
         let mut rng = rand::thread_rng();
         let color = Color::RGB(
@@ -30,41 +40,26 @@ impl Square {
             rng.gen_range(0..=255),
         );
         let rect = Rect::new(x, y, size, size);
-        let current_direction = initial_position.opposite();
-
-        // Calculate turning positions based on target_direction
-        let (turn_x, turn_y) = if target_direction == current_direction {
-            (None, None)
-        } else {
-            match target_direction {
-                Direction::Up => (Some(5 * LINE_SPACING), None),
-                Direction::Down => (Some(4 * LINE_SPACING), None),
-                Direction::Left => (None, Some(4 * LINE_SPACING)),
-                Direction::Right => (None, Some(5 * LINE_SPACING)),
-            }
-        };
-        // println!(
-        //     "Current Direction: {:?}, Target Direction: {:?}, Turn X: {:?}, Turn Y: {:?}",
-        //     current_direction, target_direction, turn_x, turn_y
-        // );
 
         Square {
             rect,
             color,
-            initial_direction: initial_position,
+            initial_direction,
             target_direction,
-            current_direction,
+            // if we initialize with UP, then our target is DOWN
+            current_direction: initial_direction.opposite(),
             turn_x,
             turn_y,
         }
     }
 
     pub fn update(&mut self) {
+        // TODO: if more than one the `turn_to_target_direction` could break
         match self.current_direction {
-            Direction::Down => self.rect.set_y(self.rect.y() + 2),
-            Direction::Up => self.rect.set_y(self.rect.y() - 2),
-            Direction::Left => self.rect.set_x(self.rect.x() - 2),
-            Direction::Right => self.rect.set_x(self.rect.x() + 2),
+            Direction::Down => self.rect.set_y(self.rect.y() + 1),
+            Direction::Up => self.rect.set_y(self.rect.y() - 1),
+            Direction::Left => self.rect.set_x(self.rect.x() - 1),
+            Direction::Right => self.rect.set_x(self.rect.x() + 1),
         }
 
         self.turn_to_target_direction();
@@ -98,18 +93,100 @@ pub fn spawn_square(squares: &mut Vec<Square>) {
     let initial_direction = Direction::new(None);
     let target_direction: Direction = Direction::new(Some(initial_direction));
 
-    let (x, y) = match initial_direction {
-        Direction::Up => (4 * LINE_SPACING, -LINE_SPACING),
-        Direction::Left => (-LINE_SPACING, 5 * LINE_SPACING),
-        Direction::Down => (5 * LINE_SPACING, WINDOW_SIZE as i32),
-        Direction::Right => (WINDOW_SIZE as i32, 4 * LINE_SPACING),
-    };
+    let calculated_coordinates = calculate_coordinates(initial_direction, target_direction);
 
     squares.push(Square::new(
-        x,
-        y,
+        calculated_coordinates.starting_x,
+        calculated_coordinates.starting_y,
         initial_direction,
         target_direction,
         SQUARE_SIZE,
+        calculated_coordinates.turn_x,
+        calculated_coordinates.turn_y,
     ));
+}
+
+fn calculate_coordinates(
+    current_direction: Direction,
+    target_direction: Direction,
+) -> CalculatedCoordinates {
+    // lanes from 4 to 10
+    match (current_direction, target_direction) {
+        (Direction::Up, Direction::Up) => unreachable!(),
+        (Direction::Up, Direction::Down) => CalculatedCoordinates {
+            starting_x: 5 * LINE_SPACING,
+            starting_y: -LINE_SPACING,
+            turn_x: None,
+            turn_y: None,
+        },
+        (Direction::Up, Direction::Left) => CalculatedCoordinates {
+            starting_x: 4 * LINE_SPACING,
+            starting_y: -LINE_SPACING,
+            turn_x: None,
+            turn_y: Some(4 * LINE_SPACING),
+        },
+        (Direction::Up, Direction::Right) => CalculatedCoordinates {
+            starting_x: 6 * LINE_SPACING,
+            starting_y: -LINE_SPACING,
+            turn_x: None,
+            turn_y: Some(7 * LINE_SPACING),
+        },
+        (Direction::Down, Direction::Up) => CalculatedCoordinates {
+            starting_x: 8 * LINE_SPACING,
+            starting_y: WINDOW_SIZE as i32,
+            turn_x: None,
+            turn_y: None,
+        },
+        (Direction::Down, Direction::Down) => unreachable!(),
+        (Direction::Down, Direction::Left) => CalculatedCoordinates {
+            starting_x: 7 * LINE_SPACING,
+            starting_y: WINDOW_SIZE as i32,
+            turn_x: None,
+            turn_y: Some(6 * LINE_SPACING),
+        },
+        (Direction::Down, Direction::Right) => CalculatedCoordinates {
+            starting_x: 9 * LINE_SPACING,
+            starting_y: WINDOW_SIZE as i32,
+            turn_x: None,
+            turn_y: Some(9 * LINE_SPACING),
+        },
+        (Direction::Left, Direction::Up) => CalculatedCoordinates {
+            starting_x: -LINE_SPACING,
+            starting_y: 7 * LINE_SPACING,
+            turn_x: Some(7 * LINE_SPACING),
+            turn_y: None,
+        },
+        (Direction::Left, Direction::Down) => CalculatedCoordinates {
+            starting_x: -LINE_SPACING,
+            starting_y: 9 * LINE_SPACING,
+            turn_x: Some(4 * LINE_SPACING),
+            turn_y: None,
+        },
+        (Direction::Left, Direction::Left) => unreachable!(),
+        (Direction::Left, Direction::Right) => CalculatedCoordinates {
+            starting_x: -LINE_SPACING,
+            starting_y: 8 * LINE_SPACING,
+            turn_x: None,
+            turn_y: None,
+        },
+        (Direction::Right, Direction::Up) => CalculatedCoordinates {
+            starting_x: WINDOW_SIZE as i32,
+            starting_y: 4 * LINE_SPACING,
+            turn_x: Some(9 * LINE_SPACING),
+            turn_y: None,
+        },
+        (Direction::Right, Direction::Down) => CalculatedCoordinates {
+            starting_x: WINDOW_SIZE as i32,
+            starting_y: 6 * LINE_SPACING,
+            turn_x: Some(6 * LINE_SPACING),
+            turn_y: None,
+        },
+        (Direction::Right, Direction::Left) => CalculatedCoordinates {
+            starting_x: WINDOW_SIZE as i32,
+            starting_y: 5 * LINE_SPACING,
+            turn_x: None,
+            turn_y: None,
+        },
+        (Direction::Right, Direction::Right) => unreachable!(),
+    }
 }
